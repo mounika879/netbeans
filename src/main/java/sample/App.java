1c143373c0b4eea3b018bbeadb0425864518b6dd
mounika879 <48467095+mounika879@users.noreply.github.com>
2019-05-01 20:20:41 +0530

    Update App.java

--- src/main/java/sample/App.java
+++ src/main/java/sample/App.java
@@ -4,9 +4,9 @@ package sample;
  * Hello world!
  *
  */
-public class App 
-
-    public static void main( String[] args )
+public class App
+{   
+   public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
